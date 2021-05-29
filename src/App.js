a0b293312903c712bd0c63cf96127a263c20d42b
Daniel Kim <kkdh1109@gmail.com>
2021-05-28 22:11:33 -0400

    create google map box with marker using api doc

--- src/App.js
+++ src/App.js
@@ -1,11 +1,51 @@
 import './App.css';
+import React, {useState, useCallback} from 'react'
+import { GoogleMap, useJsApiLoader, Marker, InfoWindow } from '@react-google-maps/api';
 
 function App() {
-  return (
-    <div className="app">
- 
-    </div>
-  );
+
+  const containerStyle = {
+    width: '500px',
+    height: '500px'
+  };
+  
+  const center = {
+    lat: -34.397,
+    lng: 150.644
+  };
+
+  const { isLoaded } = useJsApiLoader({
+    id: 'google-map-script',
+    googleMapsApiKey: "AIzaSyAMQmkgHfqVwierkO-Ryo3UXEtFM9NO2dU"
+  })
+
+  const [map, setMap] = useState(null)
+
+  const onLoad = useCallback(function callback(map) {
+    const bounds = new window.google.maps.LatLngBounds();
+    map.fitBounds(bounds);
+    setMap(map)
+  }, [])
+
+  const onUnmount = useCallback(function callback(map) {
+    setMap(null)
+  }, [])
+
+  return isLoaded ? (
+    <GoogleMap
+      mapContainerStyle={containerStyle}
+      center={center}
+      zoom={10}
+      onLoad={onLoad}
+      onUnmount={onUnmount}
+    >
+      <Marker position={center}>
+        <InfoWindow>
+          <div>hello</div>
+        </InfoWindow>
+      </Marker>
+    </GoogleMap>
+  ) : <></>
 }
 
 export default App;
